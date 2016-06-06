ca4649a74e08f10b9bd2353fea50b2a5ae4db8d1
julianMGCODE <julianmosquerag@gmail.com>
2016-06-06 09:24:55 +0200

    Rename del metodo met a suma y javadoc añadido

--- src/examenfinalparte1/Prin.java
+++ src/examenfinalparte1/Prin.java
@@ -5,12 +5,24 @@ import java.io.IOException;
 public class Prin {
 
     public static void main(String[] args) throws IOException {
+       
+        
+       /**
+        * Se crea un objeto de tipo Pirncipal y se le pasan los parametros 10 y 20
+        */
         Principal obj = new Principal(10, 20);
         int n = obj.i;
         System.out.println("Es: " + n);
+        
+        /**
+         * Se sum por si mismo devido al segundo constructor
+         */
         Principal nm = new Principal(10);
         int m = nm.i;
         System.out.println("Es: " + m);
+        
+        
+        
         Principal mn = new Principal();
         int nn = mn.i;
         System.out.println("Es: " + nn);
--- src/examenfinalparte1/Principal.java
+++ src/examenfinalparte1/Principal.java
@@ -4,19 +4,37 @@ public class Principal {
 
     int i;
 
+    /**
+     * Constructor que usa el método suma con dos parametros que se le pasa.
+     * @param j
+     * @param m 
+     */
     public Principal(int j, int m) {
-        i = met(j, m);
+        i = Suma(j, m);
     }
 
-    public Principal(int j) {
-        i = met(j, j);
+    /**
+     * Usa tabien el método suma para sumar el un solo parametro sobre si mismo
+     * @param j 
+     */
+    public  Principal(int j) {
+        i = Suma(j, j);
     }
 
-    public Principal() {
+    /**
+     * Iguala la variable i a menos 1
+     */
+    public  Principal() {
         i = -1;
     }
 
-    public int met(int x, int x1) {
+    /**
+     * Es un metodo para sumar
+     * @param x
+     * @param x1
+     * @return 
+     */
+    public int Suma(int x, int x1) {
         return x + x1;
     }
 }
